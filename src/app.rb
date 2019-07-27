7570bd98ccd212fc540dcf5a3d4912be5d0fdbc5
Lari Kovanen <lari@kovanen.se>
2019-07-27 12:39:03 +0200

    version 2

--- src/app.rb
+++ src/app.rb
@@ -1,12 +1,51 @@
 #!/usr/bin/env ruby
 require 'json'
+require 'httparty'
+
+country = ENV['country'] || "SE"
+platforms = ENV['platforms'].split(",").map(&:strip)
+# platforms = "appleMusic, spotify, google"
 
 url = `pbpaste`
-query = ARGV[0]
-icon = "icon.png"
+# url = "https://music.apple.com/se/album/caligula/1463453125?l=en"
+uri = "https://api.song.link/v1-alpha.1/links?userCountry=#{country}&url=#{url}"
+
+response=HTTParty.get(uri)
+parsedResponse = JSON.parse(response.to_s);
+euid = parsedResponse["entityUniqueId"]
 
 out = Hash.new
+out['items'] = [{"type" => "default",
+    "title" => "#{parsedResponse["entitiesByUniqueId"][euid]["artistName"]} - #{parsedResponse["entitiesByUniqueId"][euid]["title"]}",
+    "icon" => { "path" => "songlink.png" },
+    "valid" => true,
+    "subtitle" => "#{parsedResponse["pageUrl"]}",
+    "arg" => "#{parsedResponse["pageUrl"]}" }]
 
-out['items'] = [{"type" => "default", "title" => "song.link url", "icon" => { "path" => icon }, "subtitle" => "https://song.link/#{url}", "arg" => "https://song.link/#{url}" }]
+parsedResponse["linksByPlatform"].each{|key,value|
+    if platforms.include? key 
+        item = {"type" => "default",
+        "title" => "#{key}",
+        "valid" => true}
+
+        if File.exist?("#{key}.png")
+            item["icon"] = { "path" => "#{key}.png" }
+        else
+            item["icon"] = { "path" => "songlink.png" }
+        end
+
+        if value["nativeAppUriDesktop"]
+            item["subtitle"]="#{value["nativeAppUriDesktop"]}"
+            item["arg"]="#{value["nativeAppUriDesktop"]}"
+            # puts "#{key} #{value["nativeAppUriDesktop"]}"
+        else
+            item["subtitle"]="#{value["url"]}"
+            item["arg"]="#{value["url"]}"
+            # puts "#{key} #{value["url"]}"
+        end
+        out["items"].push(item)
+    end
+}
+
 
 print out.to_json
